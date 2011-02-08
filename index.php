ab73d68beed378b97b4a3aaf0a0fdd8889412a88
Lukas Prokop <admin@lukas-prokop.at>
2011-02-08 19:04:36 +0100

    Add index file.

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    /*
+    * ----------------------------------------------------------------------------
+    * "THE EMAIL-WARE LICENSE" (Revision 42.1337):
+    * <admin{at}lukas-prokop.at> wrote this file. As long as you retain this notice
+    * you can do whatever you want with this stuff. If you think this stuff is
+    * worth it, you can send me an email
+    * ----------------------------------------------------------------------------
+    *
+    *  Projekt:       datenlandkarten
+    *  Autor:         meisterluk
+    *  Datum:         11.02.06
+    *  Version:       beta
+    *  Lizenz:        Emailware
+    *
+    */
+
+    header('Location: input.php');
+?>
