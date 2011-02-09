97b518148a2df2bd2f3b879ae6170abe5b701242
Lukas Prokop <admin@lukas-prokop.at>
2011-02-09 09:22:52 +0100

    Remove test file.

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-type: image/svg+xml; charset=utf-8');
-    require_once('lib.php');
-
-    $image = 'svgs/bundeslaender.svg';
-    echo substitute($image, 'Carinthia', 'A small part of Austria',
-        3, 5, 3, array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-?>
