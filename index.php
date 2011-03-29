2178a365d2dc98ad7c3491864803042932419514
Lukas Prokop <admin@lukas-prokop.at>
2011-03-29 22:04:37 +0200

    Relicense Emailware to LGPL v3.0. Decision of our team.

--- index.php
+++ index.php
@@ -3,19 +3,27 @@
     //ini_set('display_errors', true);
 
     /*
-    * ----------------------------------------------------------------------------
-    * "THE EMAIL-WARE LICENSE" (Revision 42.1337):
-    * <admin{at}lukas-prokop.at> wrote this file. As long as you retain this notice
-    * you can do whatever you want with this stuff. If you think this stuff is
-    * worth it, you can send me an email
-    * ----------------------------------------------------------------------------
-    *
-    *  Projekt:       datenlandkarten
-    *  Autor:         meisterluk
-    *  Datum:         11.02.06
-    *  Version:       beta
-    *  Lizenz:        Emailware
-    *
+    Projekt:       datenlandkarten
+    Autor:         meisterluk
+    Datum:         11.02.06
+    Version:       beta
+    Lizenz:        LGPL v3.0
+
+    datamaps -- visualize your geo-based statistic data
+    Copyright (C) 2011  Lukas Prokop, Robert Harm, et al.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
     */
 
     $root = './';
